df7652f41185cdf68eeaee5ae96e0e8adffde83a
Gai Palyan <ChadReed020788@yandex.ru>
2021-03-08 16:24:14 +0300

    fix condition for win in loop.

--- src/Engine.php
+++ src/Engine.php
@@ -13,22 +13,23 @@ function Engine(string $description, callable $gameData): void
     $userName = prompt('May I have your name?');
     line('Hello, ' . $userName);
     line($description);
+    $correctAnswerCount = 0;
     for ($i = 0; $i < COUNT_ROUNDS; $i++) {
         $responseFromGames = call_user_func($gameData);
         $gameQuestion      = strval($responseFromGames['question']);
         $correctAnswer     = strval($responseFromGames['correctAnswer']);
-
         line('Question: ' . $gameQuestion);
         $userAnswer = prompt('Your answer');
         if (strtolower($userAnswer) === $correctAnswer) {
+            $correctAnswerCount++;
             line('Correct!');
         } else {
             line("'{$userAnswer}'" . ' is wrong answer ;(. Correct answer was ' . "'{$correctAnswer}'");
             line('Let\'s try again, ' . $userName . '!');
             break;
         }
-    }
-    if ($i === COUNT_ROUNDS) {
-        line('Congratulations, ' . $userName . '!');
+        if ($correctAnswerCount == COUNT_ROUNDS) {
+            line('Congratulations, ' . $userName . '!');
+        }
     }
 }
