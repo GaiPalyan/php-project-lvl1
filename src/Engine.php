a722324cd7007be193920c0ea4c3b95ef16507b9
Gai Palyan <ChadReed020788@yandex.ru>
2021-03-22 23:30:46 +0300

    remove $countCorrectAnswer

--- src/Engine.php
+++ src/Engine.php
@@ -13,13 +13,11 @@ function run(string $description, callable $gamesData): void
     $userName = prompt('May I have your name?');
     line('Hello, ' . $userName);
     line($description);
-    $correctAnswersCount = 0;
     for ($i = 0; $i < ROUNDS_COUNT; $i++) {
         [$gameQuestion, $correctAnswer] = call_user_func($gamesData);
         line('Question: ' . $gameQuestion);
         $userAnswer = prompt('Your answer');
         if (strtolower($userAnswer) === $correctAnswer) {
-            $correctAnswersCount++;
             line('Correct!');
         } else {
             line("'{$userAnswer}'" . ' is wrong answer ;(. Correct answer was ' . "'{$correctAnswer}'");
@@ -27,7 +25,5 @@ function run(string $description, callable $gamesData): void
             break;
         }
     }
-    if ($correctAnswersCount === ROUNDS_COUNT) {
-        line('Congratulations, ' . $userName . '!');
-    }
+    line('Congratulations, ' . $userName . '!');
 }
